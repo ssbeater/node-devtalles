c574f9b315760437cd3af9b069d5ff3c2907c82b
Gridman <santiagosuarez.beater@outlook.com>
2024-06-19 12:36:51 -0500

    Websockets section

--- 11-websockets/src/app.ts
+++ 11-websockets/src/app.ts
@@ -1,1 +1,30 @@
-console.log("Hello World!");
+import { WebSocketServer, WebSocket } from "ws";
+
+const PORT = 3001;
+
+const wss = new WebSocketServer({ port: PORT });
+
+wss.on("connection", function connection(ws) {
+  console.log("Client connected");
+
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data) {
+    const payload = JSON.stringify({
+      type: "custom-message",
+      payload: data.toString(),
+    });
+    
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(payload);
+      }
+    });
+  });
+
+  ws.on("close", function disconnected() {
+    console.log("Client disconnected!");
+  });
+});
+
+console.log(`Server started on port ${PORT}`);
